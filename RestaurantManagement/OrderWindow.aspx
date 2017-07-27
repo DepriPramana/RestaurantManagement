23e2a78a33741cf3ce57ea2a107a96f0d46dd2d6
robinabdullah <robinabdullah>
2017-07-27 23:04:07 +0600

    Add project files.

---
+++ RestaurantManagement/OrderWindow.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OrderWindow.aspx.cs" Inherits="RestaurantManagement.OrderWindow" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Fast Food Items" Font-Size="15" ></asp:Label>
+        <asp:Panel ID="foodPanel" runat="server"></asp:Panel>
+        <asp:PlaceHolder ID="food" runat="server"></asp:PlaceHolder>
+        <%--<asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="~/Pictures/3.jpg" Width="100" /> 
+        <br>    <asp:Label ID="Labe1" runat="server" style="margin-left: 25px; margin-top: 0px" Text="Burger" Font-Size="13" ></asp:Label>--%>
+    </div>
+    </form>
+</body>
+</html>
---
+++ RestaurantManagement/OrderWindow.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RestaurantManagement
+{
+    public partial class OrderWindow : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            string connectionString = @"Data Source=DESKTOP-CKU87KC\SQLSRV2012;Initial Catalog=Sample;Integrated Security=False;User ID=sa;Password=P@$$w0rd;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            
+            POSDBDataContext db = new POSDBDataContext(connectionString);
+
+            var temp = from ee in db.Products 
+                       where ee.Status != false
+                       select ee;
+            
+            foreach (var item in temp)
+            {
+                Panel panel = new Panel();
+
+                ImageButton image = new ImageButton();
+                image.ImageUrl = "~/Pictures/" + item.Image;
+                image.Width = 200;
+                foodPanel.Controls.Add(image);
+
+                //foodPanel.Controls.Add(panel);
+            }
+            foodPanel.Controls.Add(new LiteralControl("<br />"));
+            //foreach (var item in temp)
+            //{
+            //    Label label = new Label();
+            //    label.Text = item.Name;
+            //    //label.Attributes.CssStyle.Add("display", "block");
+            //    label.Attributes.CssStyle.Add("margin-left", "80px");
+            //    foodPanel.Controls.Add(label);
+
+            //    foodPanel.Controls.Add(new LiteralControl("<span>  dgfgf    </span>"));
+
+            //}
+        }
+    }
+}
